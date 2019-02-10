0fa1a52bae9fd001c6f1b7e7f816ea92071d06b1
Endi Caushi <mrendi29@gmail.com>
2019-02-09 22:56:31 -0500

    Added LogIn layout.

--- app/src/main/java/com/example/caushie/endisinstagram/LoginActivity.java
+++ app/src/main/java/com/example/caushie/endisinstagram/LoginActivity.java
@@ -2,12 +2,22 @@ package com.example.caushie.endisinstagram;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button  btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etPassword=findViewById(R.id.etPassword);
+        etUsername=findViewById(R.id.etUsername);
+        btnLogin = findViewById(R.id.btnLogin);
     }
 }
