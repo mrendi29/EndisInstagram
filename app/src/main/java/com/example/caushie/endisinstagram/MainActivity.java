5f611467392373a4f07c4044bfd2315d164be18e
Endi Caushi <mrendi29@gmail.com>
2019-02-17 23:09:14 -0500

    Fragment Manager Complete

--- app/src/main/java/com/example/caushie/endisinstagram/MainActivity.java
+++ app/src/main/java/com/example/caushie/endisinstagram/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.caushie.endisinstagram;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+
+import com.example.caushie.endisinstagram.fragments.ComposeFragment;
+import com.example.caushie.endisinstagram.fragments.PostsFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,6 +34,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+
+
         bottomNavigationView = findViewById(R.id.bottom_navigation);
 
         // Find the toolbar view inside the activity layout
@@ -40,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
 //        getSupportActionBar().setLogo(R.mipmap.nav_logo_whiteout1);
 //        getSupportActionBar().setDisplayUseLogoEnabled(true);
 
-        
+
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
 
@@ -48,23 +56,33 @@ public class MainActivity extends AppCompatActivity {
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
                 switch (item.getItemId()) {
 
                     case R.id.action_compose:
+                        //TODO: Swap fragment
+                        fragment = new ComposeFragment();
                         Toast.makeText(MainActivity.this, "Action Compose", Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.action_home:
+                        //TODO: Swap fragment
+                        fragment = new PostsFragment();
                         Toast.makeText(MainActivity.this, "Action home", Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.action_profile:
+                        //TODO: Swap fragment
+                        fragment = new ComposeFragment();
                         Toast.makeText(MainActivity.this, "Action profile", Toast.LENGTH_SHORT).show();
                     default:
-
+                        fragment = new ComposeFragment();
                         break;
                 }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
                 return true;
             }
         });
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
 
     }
 
